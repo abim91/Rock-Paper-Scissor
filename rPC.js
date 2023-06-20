67371632ab282f64ef1257c52cf9f8646afe77d7
Abim Shrestha <53064345+abim91@users.noreply.github.com>
2023-06-20 13:28:53 -0400

    workable on termi

--- rPC.js
+++ rPC.js
@@ -1,1 +1,51 @@
 console.log("hello world");
+
+function getComputerChoice(){
+    const choices = ["Rock", "Paper", "Scissor"];
+    var randChoice = Math.floor((Math.random() * 3));
+    return choices[randChoice];
+}
+let userChoice = prompt("Choose Your Weapon: Rock, Paper or Scissor");
+userChoice = userChoice.toLowerCase();
+
+let computerChoice = getComputerChoice();
+
+function playRound(userChoice, computerChoice){
+    
+
+    if(computerChoice === "Rock"){
+        if(userChoice === "rock")
+            return "It is a draw";
+        if(userChoice === "paper")
+            return "You Won";
+        if(userChoice === "scissor")
+            return "You Lost";
+        
+
+
+    }
+    if(computerChoice === "Paper"){
+        if(userChoice === "paper")
+            return "It is a draw";
+        if(userChoice === "Rock")
+            return "You Lost";
+        if(userChoice === "scissor")
+            return "You Won!!!";
+
+        
+        
+    }
+    if(computerChoice === "Scissor"){
+        
+        if(userChoice === "scissor")
+        return "It is a draw";
+        if(userChoice === "rock")
+        return "You Won!!!";
+        if(userChoice === "paper")
+        return "You Lost";
+        
+    }
+
+}
+console.log(computerChoice);
+console.log(playRound(userChoice, computerChoice));
